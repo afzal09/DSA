46ffaf600b59142b8710d2ff875e2f6525f9d8c8
Afzal <afzalmomin2003@gmail.com>
2024-01-30 17:08:51 +0530

    Binary Search

--- recurssion/BinarySearch.py
+++ recurssion/BinarySearch.py
@@ -1,23 +1,22 @@
-import math
-def Binary_Search(data, target, low, high):
-    """ returns true if target is found in indicated portion of a list the search only includes data[low] to data[high]
-    """
-    if low > high:
-        return False
-    else:
-        mid = math.floor((low + high) / 2)
-        if target == data[mid]:
-            return True
-        elif target < data[mid]:
-            print(target)
-            return Binary_Search(data, target, low, mid - 1)
-        else:
-            print(target)
-            return Binary_Search(data, target, mid + 1, high)
-        
+def Binary_search(data,target,low,high):
+	print(data,"\n",target,"\n",low,"\n",high)
+	if target < low:
+		return False
+	else:
+		mid = (low + high) // 2
+		print(mid)
+		if target == data[mid]:
+			return True
+		elif target < data[mid]:
+			return Binary_search(data, target, low, mid -1)
+		else:
+			return Binary_search(data,target,mid + 1, high)
 
-data = [10,20,30]
-target = 20
-low = data[0]
-high = data[len(data) - 1]
-Binary_Search(data, target, low, high)
+data = [10,12,14,16,18,20,22,24,26,28,30]
+target = 24
+result = Binary_search(data, target, 0, len(data) - 1)
+
+if result:
+    print(f'Target {target} found in the list data is {data}.')
+else:
+    print(f'Target {target} not found in the list data is {data}.')
